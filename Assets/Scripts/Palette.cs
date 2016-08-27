5d3f368db39420043115ca94d57e3ba2e99a21ae
Noah Ratcliff <me@nratcliff.com>
2016-08-27 13:41:32 -0400

    made player prefab with palette-definined colors

---
+++ Assets/Scripts/Palette.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+class Palette
+{
+    public static Color[] Colors =
+    {
+        new Color(193f/255f, 58f/255f, 58f/255f),
+        new Color(155f/255f, 220f/255f, 139f/255f),
+        new Color(167f/255f, 166f/255f, 193f/255f),
+        new Color(85f/255f, 153f/255f, 192f/255f)
+    };
+
+    public static Color GetColor(PaletteColor color)
+    {
+        return Colors[(int)color];
+    }
+
+    public static bool GetPaletteColor(Color c, out PaletteColor color)
+    {
+        for (int i = 0; i < Colors.Length; i++)
+        {
+            if (Colors[i] == c)
+            {
+                color = (PaletteColor)i;
+                return true;
+            }
+        }
+
+        color = PaletteColor.InvalidColor;
+        return false;
+    }
+}
+
+public enum PaletteColor
+{
+    Red,
+    Green,
+    Purple,
+    Blue,
+    InvalidColor
+}
