ecc0296048d51e4b6008bdc5753226b36f4c4d02
Noah Ratcliff <me@nratcliff.com>
2016-08-27 01:13:15 -0400

    jumping

---
+++ Assets/Scripts/PlayerControllers/BoulderPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderPlayerController : MonoBehaviour
+{
+    public int PlayerNum;
+
+    public float JumpForce;
+
+    private bool onGround = true;
+    private Vector3 initLocalPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        initLocalPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("P" + PlayerNum + " Action") > 0 && onGround && GetComponent<Rigidbody>().velocity == Vector3.zero)
+        {
+            //jump
+            GetComponent<Rigidbody>().AddForce(transform.up * JumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+        else if (!onGround)
+        {
+            onGround = (GetComponent<Rigidbody>().velocity == Vector3.zero);
+        }
+    }
+}
