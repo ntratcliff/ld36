1000181ed5aa778d020785d96e086f9436fb061a
Noah Ratcliff <me@nratcliff.com>
2016-08-28 17:02:26 -0400

    set up main menu ui

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ChangeScene : MonoBehaviour
+{
+    [HideInInspector]
+    public int SceneIndex;
+
+    public void LoadLevel()
+    {
+        LoadLevel(SceneIndex);   
+    }
+
+    public void LoadLevel(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
+
+#if UNITY_EDITOR
+//Custom editor code for scene drop down list
+[CustomEditor(typeof(ChangeScene))]
+public class SceneDropDown : Editor
+{
+    List<string> names;
+    int choiceIndex;
+
+    void OnEnable()
+    {
+        choiceIndex = serializedObject.FindProperty("SceneIndex").intValue;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        //update scenes list
+        names = new List<string>();
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            if (EditorBuildSettings.scenes[i].enabled)
+                names.Add(Path.GetFileNameWithoutExtension(EditorBuildSettings.scenes[i].path));
+        }
+
+        //display choice UI elements
+        choiceIndex = EditorGUILayout.Popup("Scene:", choiceIndex, names.ToArray());
+
+        //set values
+        ChangeScene obj = target as ChangeScene;
+        obj.SceneIndex = choiceIndex;
+        EditorUtility.SetDirty(target);
+    }
+}
+#endif
---
+++ Assets/Scripts/UI/Bob.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bob : MonoBehaviour
+{
+    public float MinScale;
+    public float MaxScale;
+    public bool StartMax;
+    public float LerpTime;
+    public float Tolerance;
+
+    private float currentScale;
+    private float targetScale;
+    private float currentStep;
+    private float fromScale;
+    private Vector3 rootScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        rootScale = GetComponent<RectTransform>().localScale;
+        targetScale = MaxScale;
+        currentScale = MinScale;
+
+        if(StartMax)
+        {
+            targetScale = MinScale;
+            currentScale = MaxScale;
+        }
+
+
+        fromScale = currentScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate step
+        float t = Time.deltaTime / LerpTime;
+        currentStep += t;
+
+        //calculate scale
+        currentScale = Mathf.Lerp(fromScale, targetScale, currentStep);
+
+        if (targetScale == MinScale && currentScale - Tolerance <= targetScale)
+        {
+            targetScale = MaxScale;
+            fromScale = MinScale;
+            currentStep = 0f;
+        }
+        else if (targetScale == MaxScale && currentScale + Tolerance >= targetScale)
+        {
+            targetScale = MinScale;
+            fromScale = MaxScale;
+            currentStep = 0f;
+        }
+
+        //update transform
+        GetComponent<RectTransform>().localScale = rootScale * currentScale;
+    }
+}
