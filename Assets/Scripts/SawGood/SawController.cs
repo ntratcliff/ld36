41ee18c55e02a0e54e9e20d26e1dd16f1bfd2cb5
Noah Ratcliff <me@nratcliff.com>
2016-08-28 12:22:02 -0400

    add saw slerp and alternating position

--- Assets/Scripts/SawGood/SawController.cs
+++ Assets/Scripts/SawGood/SawController.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class SawController : MonoBehaviour
 {
+    [Tooltip("How far the saw moves from center with each trigger press")]
+    public float SawOffset;
+
+    public float SlerpSpeed;
+
     private PlayerScore playerScore;
     private PlayerInfo playerInfo;
     private SawGoodGame gameController;
 
     private float topY;
+    private float targetX;
 
     private bool positive = true;
 
@@ -24,16 +30,27 @@ public class SawController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (positive && playerInfo.GetAxis("Triggers") > 0
-            || !positive && playerInfo.GetAxis("Triggers") < 0)
+        float triggers = playerInfo.GetAxis("Triggers");
+        if (positive && triggers > 0
+            || !positive && triggers < 0)
         {
             sawOnce();
         }
 
+        Vector3 localPos = transform.localPosition;
+
+        //set x pos relative to depressed trigger
+        if (triggers != 0)
+            localPos = transform.right * Mathf.Sign(triggers) * SawOffset;
+
         //set y pos relative to score
-        Vector3 pos = transform.position;
-        pos.y = topY * (gameController.TargetScore - playerScore.Score) / gameController.TargetScore;
-        transform.position = pos;
+        localPos.y = topY * (gameController.TargetScore - playerScore.Score) / gameController.TargetScore;
+
+        //slerp position
+        transform.localPosition = Vector3.Slerp(
+            transform.localPosition,
+            localPos,
+            Time.deltaTime * SlerpSpeed);
     }
 
     private void sawOnce()
@@ -42,6 +59,7 @@ public class SawController : MonoBehaviour
         positive = !positive;
 
         //update score
+        if(playerScore.Score < gameController.TargetScore)
         playerScore.Score++;
     }
 }
