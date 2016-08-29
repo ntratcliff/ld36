7a00b923fe28d988524055126a7a94b1a0f21c43
Noah Ratcliff <me@nratcliff.com>
2016-08-29 00:53:43 -0400

    fixed delay on EggsInABasket

--- Assets/Scripts/EggsInABasket/DelayStartEggs.cs
+++ Assets/Scripts/EggsInABasket/DelayStartEggs.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DelayStart : MonoBehaviour {
+public class DelayStartEggs : MonoBehaviour
+{
     public float startDelay;
     private float timeSinceStart;
     private GameObject[] players;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         timeSinceStart = 0;
         gameObject.GetComponent<Scoreboard>().UseTimer = false;
         gameObject.GetComponent<RandomSpawner>().enabled = false;
         players = GameObject.FindGameObjectsWithTag("Player");
 
-        for(int i = 0; i < players.Length; i++)
+        for (int i = 0; i < players.Length; i++)
         {
             players[i].GetComponent<JoystickMovement>().enabled = false;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         timeSinceStart += Time.deltaTime;
 
-        if(timeSinceStart > startDelay)
+        if (timeSinceStart > startDelay)
         {
             gameObject.GetComponent<Scoreboard>().UseTimer = true;
             gameObject.GetComponent<RandomSpawner>().enabled = true;
@@ -31,5 +34,5 @@ public class DelayStart : MonoBehaviour {
                 players[i].GetComponent<JoystickMovement>().enabled = true;
             }
         }
-	}
+    }
 }
