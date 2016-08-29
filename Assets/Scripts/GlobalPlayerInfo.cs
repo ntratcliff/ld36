2cc28b65c5ef3f0afde4eee598f44ac0e2449ac7
Noah Ratcliff <me@nratcliff.com>
2016-08-28 20:28:25 -0400

    player join screen

---
+++ Assets/Scripts/GlobalPlayerInfo.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class GlobalPlayerInfo : MonoBehaviour
+{
+    public GameObject PlayerInfoPrefab;
+
+    private List<GameObject> players;
+    private GameObject[] playersInScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        players = new List<GameObject>();
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        playersInScene = GetPlayersInScene();
+        disableInactivePlayers();
+        setPlayerInfoInScene();
+    }
+
+    /// <summary>
+    /// Disables any players in the scene 
+    /// who don't have a player in real life
+    /// </summary>
+    private void disableInactivePlayers()
+    {
+        for (int i = players.Count() - 1; i < playersInScene.Length; i++)
+        {
+            playersInScene[i].SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Sets player information for players in the scene
+    /// </summary>
+    private void setPlayerInfoInScene()
+    {
+        for (int i = 0; i < playersInScene.Length; i++)
+        {
+            //set color
+            playersInScene[i].GetComponent<CharacterColor>().Color = players[i].GetComponent<CharacterColor>().Color;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddPlayer(int p)
+    {
+        GameObject playerinfo = GameObject.Instantiate(PlayerInfoPrefab);
+        playerinfo.GetComponent<PlayerInfo>().PlayerNum = p;
+        playerinfo.GetComponent<CharacterColor>().Color = (PaletteColor)p;
+        playerinfo.name = "P" + p;
+
+        playerinfo.transform.parent = this.transform;
+
+        players.Add(playerinfo);
+
+        //sort players
+        players = players.OrderBy(x => x.GetComponent<PlayerInfo>().PlayerNum).ToList();
+    }
+
+    public void RemovePlayer(int p)
+    {
+        GameObject obj = players.Find(x => x.GetComponent<PlayerInfo>().PlayerNum == p);
+        players.Remove(obj);
+        GameObject.Destroy(obj);
+    }
+
+    public bool HasPlayer(int p)
+    {
+        if (players.Find(x => x.GetComponent<PlayerInfo>().PlayerNum == p))
+            return true;
+        return false;
+    }
+
+    public GameObject[] GetPlayersInScene()
+    {
+        return GameObject.FindGameObjectsWithTag("Player")
+            .OrderBy(x => x.GetComponent<PlayerInfo>().PlayerNum).ToArray();
+    }
+}
---
+++ Assets/Scripts/PlayerJoin.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerJoin : MonoBehaviour
+{
+    public string JoinAxis;
+    public string ExitAxis;
+    public int Player;
+
+    private GlobalPlayerInfo gPlayerInfo;
+    private bool playerJoined;
+    private bool buttonDown;
+
+    // Use this for initialization
+    void Start()
+    {
+        gPlayerInfo = FindObjectOfType<GlobalPlayerInfo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!buttonDown && !playerJoined 
+            && Input.GetAxis("P" + Player + " " + JoinAxis) != 0)
+        {
+            transform.FindChild("JoinText").gameObject.SetActive(false);
+            transform.FindChild("NameText").gameObject.SetActive(true);
+            gPlayerInfo.AddPlayer(Player);
+            playerJoined = true;
+        }
+        else if (!buttonDown && playerJoined 
+            && Input.GetAxis("P" + Player + " " + ExitAxis) != 0)
+        {
+            transform.FindChild("JoinText").gameObject.SetActive(true);
+            transform.FindChild("NameText").gameObject.SetActive(false);
+            gPlayerInfo.RemovePlayer(Player);
+            playerJoined = false;
+        }
+        else if (!buttonDown && !playerJoined
+            && Player == 1 && Input.GetAxis("P1 " + ExitAxis) != 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        buttonDown = (Input.GetAxis("P" + Player + " " + JoinAxis) != 0
+            || Input.GetAxis("P" + Player + " " + ExitAxis) != 0);
+
+    }
+}
