700af0842ee2f5dbfad57ac2328d5531ccb4c993
Noah Ratcliff <me@nratcliff.com>
2016-08-29 12:08:13 -0400

    forgot to add SecondaryLoop.cs to b1ad316

---
+++ Assets/Scripts/SecondaryLoop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondaryLoop : MonoBehaviour
+{
+    public AudioClip LoopClip;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        //get attached source
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!source.isPlaying)
+        {
+            source.clip = LoopClip;
+            source.loop = true;
+            source.Play();
+        }
+    }
+}
