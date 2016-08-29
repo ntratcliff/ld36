0d0c7cd719233cde19daf75f4503fe237f7895b3
Aidan Markham <apm4337@g.rit.edu>
2016-08-29 01:00:44 -0400

    Added Delay to Saw Good

---
+++ Assets/Scripts/SawGood/DelayStartSaw.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayStartSaw : MonoBehaviour
+{
+    public float startDelay;
+    private float timeSinceStart;
+    private GameObject[] players;
+    // Use this for initialization
+    void Start()
+    {
+        timeSinceStart = 0;
+
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].transform.Find("Saw").GetComponent<SawController>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+
+        if (timeSinceStart > startDelay)
+        {
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].transform.Find("Saw").GetComponent<SawController>().enabled = true;
+            }
+        }
+    }
+}
