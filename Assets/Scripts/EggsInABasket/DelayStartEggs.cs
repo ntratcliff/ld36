5137de183f16e564e603e612df39a85742974745
Aidan Markham <apm4337@g.rit.edu>
2016-08-29 00:40:30 -0400

    Gave Eggs in a Basket a countdown

---
+++ Assets/Scripts/EggsInABasket/DelayStartEggs.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayStart : MonoBehaviour {
+    public float startDelay;
+    private float timeSinceStart;
+    private GameObject[] players;
+	// Use this for initialization
+	void Start () {
+        timeSinceStart = 0;
+        gameObject.GetComponent<Scoreboard>().UseTimer = false;
+        gameObject.GetComponent<RandomSpawner>().enabled = false;
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for(int i = 0; i < players.Length; i++)
+        {
+            players[i].GetComponent<JoystickMovement>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceStart += Time.deltaTime;
+
+        if(timeSinceStart > startDelay)
+        {
+            gameObject.GetComponent<Scoreboard>().UseTimer = true;
+            gameObject.GetComponent<RandomSpawner>().enabled = true;
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].GetComponent<JoystickMovement>().enabled = true;
+            }
+        }
+	}
+}
