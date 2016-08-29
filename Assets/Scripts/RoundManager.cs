bd48e70848d5617c4ead9461fefd0ebb4947c80d
Noah Ratcliff <me@nratcliff.com>
2016-08-29 17:07:26 -0400

    round management

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RoundManager : MonoBehaviour
+{
+    private int rounds = 3;
+    private int currentRound = 1;
+
+    public int CurrentRound
+    {
+        get { return currentRound; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        if (SceneManager.GetActiveScene().name == "Scoreboard" && currentRound == rounds)
+        {
+            //end of 3 rounds, set main menu as next scene
+            FindObjectOfType<NextScene>().SceneName = "MainMenu";
+        }
+
+        if (SceneManager.GetActiveScene().name == "Scoreboard")
+        {
+            currentRound++;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/UpdateTextByRound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateTextByRound : MonoBehaviour
+{
+    public string[] RoundText;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RoundManager rm = FindObjectOfType<RoundManager>();
+        if (rm != null)
+            GetComponent<Text>().text = RoundText[rm.CurrentRound - 2];
+        else
+            GetComponent<Text>().text = "";
+
+    }
+}
